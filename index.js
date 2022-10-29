b9cf1599bd26ecd89b5cb367b551fd2fedb89163
Flávio Henrique <chakcpps@gmail.com>
2022-10-29 13:47:42 -0300

    :bulb: docs: Comentando arquivo

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+//Selecionando botão  e lista não ordenada
 const botaoGerar = document.querySelector("#botaoGerar");
 
 const lista = document.querySelector("#lista");
 
-botaoGerar.addEventListener("click", function gerarNumero() {
+//Função será executada apenas quando o botão for clickado
+botaoGerar.addEventListener("click", gerarNumero);
+
+//Função para gerar números aleatórios
+function gerarNumero() {
+  //Atribuindo a variável um numero inteiro e real
   let numeroAleatorio = Math.floor(Math.random() * 100);
 
+  //Criando elemento li e dando uma classe para ele
   let itemLista = document.createElement("li");
   itemLista.classList.add("item-lista");
-
+  //Atribuindo o numero gerado ao conteudo do elemento li
   itemLista.textContent = numeroAleatorio;
 
+  //Elemento adicionado antes do último (na frente, como uma iserção em pilha)
   lista.prepend(itemLista);
-});
+}
